5bbdf2ffc6229d37e819337c0f38fd40d017b451
hansj <june12300039@gmail.com>
2025-08-28 21:06:58 +0900

    style: 커뮤니티 페이지 제목 스타일 수정 - 글씨 두께와 크기 변경

--- client/app/community/page.tsx
+++ client/app/community/page.tsx
@@ -8,7 +8,7 @@ export default function CommunityPage() {
     <div className="min-h-screen max-h-screen flex flex-col bg-[#FAF6EC] overflow-hidden"> {/* 배경색을 #FAF6EC로 변경 */}
       {/* 스크롤 가능한 컨텐츠 영역 */}
       <div className="flex-1 overflow-y-auto p-[18px] pb-[100px]"> {/* Footer 공간 확보를 위한 bottom padding */}
-        <h1 className="pl-[7px] text-[#023735] font-medium text-[20px]">Community Page</h1>
+        <h1 className="pl-[7px] text-[#023735] font-bold text-[24px]">Community Page</h1>
 
         {/* 내가 기르는 식물 리스트 */}
         <MyPlantsList />
