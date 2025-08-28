a35842f7dcf31ffa4448bd9fb064c14571498eee
hansj <june12300039@gmail.com>
2025-08-28 17:18:08 +0900

    client, 서버 폴더 분리

---
+++ client/README.md
@@ -0,0 +1,6 @@
+# 식물 기반 커뮤니티 앱
+
+## api 리스트
+
+- plant.id - <https://plant.id/docs>
+- kakao api - <https://developers.kakao.com/docs/latest/ko/getting-started/quota>
---
+++ client/app/profile/page.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export default function Page(){
+  const SERVER_URL = '';
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    // fetch req to SERVER_URL
+    fetch(SERVER_URL)
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+  return (
+    <div>{data ? JSON.stringify(data) : "Loading..."}</div>
+  );
+}
